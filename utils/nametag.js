8c69964a8165e2ab60ed21f31d34f6705965d4b7
jiinjung <jiinrosejung@gmail.com>
2022-05-19 23:28:26 -0700

    changed font styles

--- utils/nametag.js
+++ utils/nametag.js
@@ -1,15 +1,15 @@
 export const nametag = [
-    { name: "Control Monitor", x:  0.95, y:  0.3 , z : 0.95},
-    { name: "Squid Boxes", x:  0.95, y:  0.2 , z : 0.95},
-    { name: "Oscilloscope", x:  0.70, y:  0.2 , z : 0.98},
-    { name: "RAPID 2G Superconducting Rock Magnetometer", x:  0.165, y:  0.6 , z : 0.4},
-    { name: "AF Demagnetization Coil", x:  0.165, y:  0.1 , z : 0.4},
+    { name: "CONTROL MONITOR", x:  0.95, y:  0.3 , z : 0.95},
+    { name: "SQUID BOXES", x:  0.95, y:  0.2 , z : 0.95},
+    { name: "OSCILLOSCOPE", x:  0.70, y:  0.2 , z : 0.98},
+    { name: "RAPID 2G SUPERCONDUCTING ROCK MAGNETOMETER", x:  0.165, y:  0.6 , z : 0.4},
+    { name: "AF DEMAGNETIZATION COIL", x:  0.165, y:  0.1 , z : 0.4},
     { name: "IRM", x:  0.165, y:  0.24 , z : 0.04},
-    { name: "Cold Head", x:  0, y:  0.1 , z : 0.0},
-    { name: "Power Supply", x:  - 0.3 , y:  0.3 , z : 0.1},
-    { name: "Oven Controller Box", x:  - 0.25 , y:  0.27 , z : -0.006},
-    { name: "Thermal Demagnetizer", x:  - 0.27, y:  0 , z : - 0.1},
-    { name: "Microscope", x: - 0.4, y:  0.3 , z : - 1.0},
-    { name: "Weight", x: - 0.8, y:  0.24 , z : - 0.8},
-    { name: "Non Magnetic Tools", x: - 0.67, y:  0.12 , z : - 1}
+    { name: "COLD HEAD", x:  0, y:  0.1 , z : 0.0},
+    { name: "POWER SUPPLY", x:  - 0.3 , y:  0.3 , z : 0.1},
+    { name: "OVEN CONTROLLER BOX", x:  - 0.25 , y:  0.27 , z : -0.006},
+    { name: "THERMAL DEMAGNETIZER", x:  - 0.27, y:  0 , z : - 0.1},
+    { name: "MICROSCOPE", x: - 0.4, y:  0.3 , z : - 1.0},
+    { name: "WEIGHT", x: - 0.8, y:  0.24 , z : - 0.8},
+    { name: "NON MAGNETIC TOOLS", x: - 0.67, y:  0.12 , z : - 1}
 ];
